5bf91c270205b9d38e9afc8c3d5fd8dad21adc5d
Yulchiko <Yulchik.82@i.ua>
2021-11-15 21:09:43 +0300

    new

---
+++ Classwork/ClassworK3/index.js
@@ -0,0 +1,60 @@
+//зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
+//   1. перебрати його циклом while
+let numbers = [2,17,13,6,22,31,45,66,100,-18];
+i = numbers.length-1;
+while (i >= 0){
+    i--;
+    console.log(numbers[i]);
+}
+//   2. перебрати його циклом for
+for (let  i = numbers.length-1; i >= 0; i--){
+    console.log(numbers[i]);
+}
+//    3. перебрати циклом while та вивести  числа тільки з непарним індексом
+
+i = numbers.length-1;
+while (i >= 0){
+    if (i%2 !== 0){
+        i--;
+        console.log(numbers [i])
+    }
+}
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+
+i = numbers.length-1;
+while (i >= 0){
+    if (i%2 !== 1){
+        i--;
+        console.log(numbers [i])
+    }
+}
+//  5. перебрати циклом while та вивести  числа тільки парні  значення
+
+for (let  i = numbers.length-1; i >= 0; i--){
+    if (i%2 === 0){
+        console.log(numbers [i]);
+
+    }
+}
+//  6. перебрати циклом for та вивести  числа тільки парні  значення
+
+for (let  i = numbers.length-1; i >= 0; i--){
+    if (i%2 === 0){
+        console.log(numbers [i]);
+    }
+}
+//  7. замінити кожне число кратне 3 на слово "okten"
+
+for (let  i = numbers.length-1; i >= 0; i--){
+    if (numbers[i]%3 === 0){
+        numbers[i] = "okten";
+        console.log(numbers[i]);
+    }
+}
+console.log(numbers);
+//   8. вивести масив в зворотньому порядку.
+
+for (let i = numbers.length-1; i >= 0; i--){
+    console.log(numbers[i]);
+}
+//   9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
