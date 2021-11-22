deb1453e7d1208f98cd4a2b410c5b16316d18aa1
Yulchiko <Yulchik.82@i.ua>
2021-11-22 23:05:49 +0300

    new

---
+++ Classwork/Classwork5/Classwork5.js
@@ -0,0 +1,107 @@
+//Всі функції повинні бути описані стрілочним типом!!!!
+//    - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+let numMin = (num1, num2, num3) => {
+    if (num1 < num2 && num1 < num3) {
+        console.log(num1);
+    } else if (num2 < num3 && num2 < num1) {
+        console.log(num2);
+    } else {
+        console.log(num3);
+    }
+}
+numMin(8, 20, 125);
+//- створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+let numMax = (num1, num2, num3) => {
+    if (num1 > num2 && num1 > num3) {
+              console.log(num1);
+    } else if (num2 > num3 && num2 > num1) {
+        console.log(num2);
+    } else {
+        console.log(num3);
+    }
+}
+numMax(8, 20, 125);
+//- створити функцію яка повертає найбільше число з масиву
+
+const funMax = (array)=> {
+    let max = array[0];
+    for (const funMaxElement of array) {
+        if (funMaxElement > max) max = funMaxElement;
+    }
+    return max;
+};
+const funMaxReturn = funMax([2,3,4,5,6,10,4,3,6]);
+console.log(funMaxReturn);
+//- створити функцію яка повертає найменьше число з масиву
+const funMin1 = (array)=> {
+    let min = array[0];
+    for (const funMaxElement of array) {
+        if (funMaxElement < min) min = funMaxElement;
+    }
+    return min;
+};
+const funMin1Return1 = funMin1([2,3,4,5,6,10,4,3,6]);
+console.log(funMin1Return1);
+//- створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+let summArray = [1,2,3,4]; // 10
+let sumElement = (array) =>{
+    let sum = 0;
+    for (const arrayElement of array) {
+        sum = arrayElement + sum;
+    }
+    return sum;
+}
+console.log(sumElement(summArray))
+//- створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+let seredArray = [1,2,3]; // 6/3=2
+let sered = (array) =>{
+    let sum = 0;
+    for (const arrayElement of array) {
+              sum = arrayElement + sum;
+    }
+    return sum/array.length;
+}
+console.log(sered(seredArray))
+//- створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+let list = [34, 1, 529, -78, 89.7, 1243, 2000, 300000];
+let maxElement = (list)=>{
+    let max;
+    let min = list[0];
+    for(let i = 0; i < list.length; i++){
+        if((list[i]) < min){
+            min = list[i];
+        } else {
+            max = list[i];
+        }
+    }
+    return max;
+}
+console.log(maxElement(list));
+//- створити функцію яка заповнює масив рандомними числами
+//(цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+let randomW = (limit) => {
+    for (let i = 0; i < 100; i++){
+        let t = Math.round(Math.random()*limit);
+        document.write(`<div>${t}</div>`);
+    }
+}
+randomW(100);
+//- створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+let i = 0;
+let randomWrite = (i, limit) => {
+    i++;
+    console.log(i);
+    if (i > limit){
+        return
+    }
+    randomWrite (0, 25);
+}
+//- Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+let reverseArr = (arr) =>{
+    let outArr = [];
+    for (let i = arr.length-1;i>=0;i--){
+        outArr.push(arr[i]);
+    }
+    return outArr;
+}
+console.log(reverseArr([1,2,3]));
